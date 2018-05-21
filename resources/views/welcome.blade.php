0569440626cf7bea1c14d7e931df64b499c08fe5
Hoang Hung <quochung0329@gmail.com>
2018-05-21 22:04:03 +0700

    create frame for project

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,21 +1,17 @@
 <!DOCTYPE html>
 <html>
+<title>Minh Long</title>
+<link rel="stylesheet" href="{{URL::asset('css/bootstrap.css')}}">
+<link rel="stylesheet" href="{{URL::asset('css/main.css')}}">
+<script src="{{ URL::asset("/js/jquery.js")}}"></script>
 <script src="{{ URL::asset("/js/angular.min.js")}}"></script>
+<script src="{{ URL::asset("/js/particles.js")}}"></script>
+
 <body>
+@include('layout.header')
 
-<div ng-app="myapp">
-    <div ng-controller="HeaderController">
-        <h2>Xin chào @{{data.title}} đến với website @{{data.website}}!</h2>
-    </div>
-</div>
-<script>
-    angular.module("myapp", []).controller("HeaderController", function($scope) {
 
-        $scope.data = {
-            title : 'các bạn',
-            website : 'freetuts.net'
-        };
-    });
-</script>
+@include('layout.footer')
+<script src="{{ URL::asset("/js/bootstrap.js")}}"></script>
 </body>
 </html>
