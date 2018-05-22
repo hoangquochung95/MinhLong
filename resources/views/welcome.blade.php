b3de68ad38a40daea5a7da3a5e6de7e76fa588c7
Hoang Hung <quochung0329@gmail.com>
2018-05-22 22:27:32 +0700

    create frame for project

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,1 @@
-<!DOCTYPE html>
-<html>
-<title>Minh Long</title>
-<link rel="stylesheet" href="{{URL::asset('css/bootstrap.css')}}">
-<link rel="stylesheet" href="{{URL::asset('css/main.css')}}">
-<script src="{{ URL::asset("/js/jquery.js")}}"></script>
-<script src="{{ URL::asset("/js/angular.min.js")}}"></script>
-<script src="{{ URL::asset("/js/particles.js")}}"></script>
-
-<body>
-@extends ('layout.header')
-@extends ('layout.content')
-@extends ('layout.footer')
-<script src="{{ URL::asset("/js/bootstrap.js")}}"></script>
-</body>
-</html>
+@extends('layout.master')
