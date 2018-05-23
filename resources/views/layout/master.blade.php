069d8d774555339d7f301fc98a32fa3b47b0cf11
Hoang Hung <quochung0329@gmail.com>
2018-05-23 22:32:14 +0700

    add particles js

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -5,13 +5,25 @@
 <link rel="stylesheet" href="{{URL::asset('css/main.css')}}">
 <script src="{{ URL::asset("/js/jquery.js")}}"></script>
 <script src="{{ URL::asset("/js/angular.min.js")}}"></script>
-<script src="{{ URL::asset("/js/particles.js")}}"></script>
-
 <body>
 
-@extends ('layout.header')
-@extends ('layout.content')
+@include ('layout.header')
+<div id="particles-js">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+
+            </div>
+            <div class="col-md-8">
+
+            </div>
+
+        </div>
+    </div>
+</div>
 @extends ('layout.footer')
 <script src="{{ URL::asset("/js/bootstrap.js")}}"></script>
+<script src="{{ URL::asset("/js/particles.js")}}"></script>
+<script src="{{ URL::asset("/js/main.js")}}"></script>
 </body>
 </html>
