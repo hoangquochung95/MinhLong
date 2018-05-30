9e1dae896c8f3ea47a78a9e38f629f373141ea57
Hoang Hung <quochung0329@gmail.com>
2018-05-30 21:58:26 +0700

    Add modules test

---
+++ resources/views/component/CategoryList/index.blade.php
@@ -0,0 +1,11 @@
+<div ng-app="categoryList">
+    <div class="list-group">
+        <span class="list-group-item disabled">
+           <p id="titleCategoryList"> Thể Loại</p>
+        </span>
+        <a href="#" class="list-group-item">Dapibus ac facilisis in</a>
+        <a href="#" class="list-group-item">Morbi leo risus</a>
+        <a href="#" class="list-group-item">Porta ac consectetur ac</a>
+        <a href="#" class="list-group-item">Vestibulum at eros</a>
+    </div>
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,15 @@
 @extends('layout.master')
-@section('master_container_left')
-aa
+@section('master_container')
+
+    <div class="main_class container @yield('master_div_class')" id ="particles-js">
+        <div class="row">
+            <div class="col-md-4">
+                @include ('component.CategoryList.index')
+            </div>
+            <div class="col-md-8">
+
+            </div>
+
+        </div>
+    </div>
 @endsection
