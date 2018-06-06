59421a8ca2de3534d1451ecf0393057a0c399758
Hoang Hung <quochung0329@gmail.com>
2018-06-06 22:41:47 +0700

    Create index website

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -272,16 +272,39 @@
     <p class="view-tab-title"> abc</p>
     <div class="row view-tab highlights-item-shop">
         <div class="col-md-3">
-            <img class="card-img-top" src="{{ URL::asset('image/b.jpeg') }}" alt="Card image cap">
+            <img class="card-img-top large-item-shop" src="{{ URL::asset('image/b.jpeg') }}" alt="Card image cap">
+            <img class="card-img-top small-item-shop" src="{{ URL::asset('image/b.jpeg') }}" alt="Card image cap">
         </div>
         <div class="col-md-3">
-            <img class="card-img-top" src="{{ URL::asset('image/b.jpeg') }}" alt="Card image cap">
+            <img class="card-img-top small-item-shop" src="{{ URL::asset('image/b.jpeg') }}" alt="Card image cap">
+            <img class="card-img-top large-item-shop" src="{{ URL::asset('image/b.jpeg') }}" alt="Card image cap">
         </div>
         <div class="col-md-3">
-            <img class="card-img-top" src="{{ URL::asset('image/b.jpeg') }}" alt="Card image cap">
+            <img class="card-img-top large-item-shop" src="{{ URL::asset('image/b.jpeg') }}" alt="Card image cap">
+            <img class="card-img-top small-item-shop" src="{{ URL::asset('image/b.jpeg') }}" alt="Card image cap">
         </div>
         <div class="col-md-3">
-            <img class="card-img-top" src="{{ URL::asset('image/b.jpeg') }}" alt="Card image cap">
+            <img class="card-img-top small-item-shop" src="{{ URL::asset('image/b.jpeg') }}" alt="Card image cap">
+            <img class="card-img-top large-item-shop" src="{{ URL::asset('image/b.jpeg') }}" alt="Card image cap">
+        </div>
+
+    </div>
+    <div class="row view-tab">
+        <div class="col-md-3">
+            <img class="card-img-top large-item-shop" src="{{ URL::asset('image/b.jpeg') }}" alt="Card image cap">
+            <img class="card-img-top small-item-shop" src="{{ URL::asset('image/b.jpeg') }}" alt="Card image cap">
+        </div>
+        <div class="col-md-3">
+            <img class="card-img-top small-item-shop" src="{{ URL::asset('image/b.jpeg') }}" alt="Card image cap">
+            <img class="card-img-top large-item-shop" src="{{ URL::asset('image/b.jpeg') }}" alt="Card image cap">
+        </div>
+        <div class="col-md-3">
+            <img class="card-img-top large-item-shop" src="{{ URL::asset('image/b.jpeg') }}" alt="Card image cap">
+            <img class="card-img-top small-item-shop" src="{{ URL::asset('image/b.jpeg') }}" alt="Card image cap">
+        </div>
+        <div class="col-md-3">
+            <img class="card-img-top small-item-shop" src="{{ URL::asset('image/b.jpeg') }}" alt="Card image cap">
+            <img class="card-img-top large-item-shop" src="{{ URL::asset('image/b.jpeg') }}" alt="Card image cap">
         </div>
 
     </div>
