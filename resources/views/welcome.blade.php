c76a2b0bb2c734cbf43b5d9731c649a1ceb92977
Hoang Hung <quochung0329@gmail.com>
2018-06-14 22:47:16 +0700

    Create index website

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -325,6 +325,43 @@
         </div>
 
     </div>
+    <p class="view-tab-title">Liên Hệ</p>
+    <div class="row view-tab contact-index">
+        <div class="col-md-6 avatar-index" style="width: 50%">
+            <img src="{{ URL::asset('image/b.jpeg') }}" alt="Avatar" class="image">
+            <div class="avatar-index-body">
+                <div class="avatar-index-text">Hello World</div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <form action="">
+                <div class="form-group">
+                    <label for="">Họ tên</label>
+                    <input type="text" class="form-control" placeholder="Enter email">
+                </div>
+                <div class="form-group">
+                    <label for="">Email</label>
+                    <input type="text" class="form-control" placeholder="Enter email">
+                </div>
+                <div class="form-group">
+                    <label for="">Số điện thoại</label>
+                    <input type="text" class="form-control" placeholder="Enter email">
+                </div>
+                <div class="form-group">
+                    <label for="">Địa chỉ</label>
+                    <input type="text" class="form-control" placeholder="Enter email">
+                </div>
+                <div class="form-group">
+                    <label for="">Ghi chú</label>
+                    <textarea type="text" class="form-control" placeholder="Enter email"></textarea>
+                </div>
+                <div class="form-group">
+                    <button class="btn btn-success">active</button>
+                    <button class="btn">cancel</button>
+                </div>
+            </form>
+        </div>
+    </div>
 </div>
 
 @endsection
