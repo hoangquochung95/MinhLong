0ee43f720558d6889a5962067fb6d2eefd964357
hoangquochung95 <quochung0329@gmail.com>
2018-06-19 09:21:03 +0700

    Create index website

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,72 @@
-<!DOCTYPE html><html><head><meta charset=utf-8><meta name=viewport content="width=device-width,initial-scale=1"><title>resources</title><link href=/static/css/app.cca059254702f9ed953b7df749673cf4.css rel=stylesheet></head><body><div id=app></div><script type=text/javascript src=/static/js/manifest.2ae2e69a05c33dfc65f8.js></script><script type=text/javascript src=/static/js/vendor.7fed9fa7b7ba482410b7.js></script><script type=text/javascript src=/static/js/app.f16ac5c624284d30f5af.js></script></body></html>
+@extends('layout.master')
+@section('master_container')
+    <div class="container" >
+        <div class="row">
+            <div id="carousel-pr-index" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img class="d-block w-100" src="{{ URL::asset('image/a.jpeg') }}" alt="First slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="{{ URL::asset('image/b.jpeg') }}" alt="Second slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="{{ URL::asset('image/c.jpeg') }}" alt="Third slide">
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carousel-pr-index" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carousel-pr-index" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+        </div>
+        <div class="row" >
+            <div class="category-list-popular" >
+                <div class="popular-item" >
+                    <ul>
+                        <li><img src="{{ URL::asset('image/c.jpeg') }}" alt="Third slide" height="200" width="200"></li>
+                        <li><img src="{{ URL::asset('image/c.jpeg') }}" alt="Third slide" height="200" width="200"></li>
+                        <li><img src="{{ URL::asset('image/c.jpeg') }}" alt="Third slide" height="200" width="200"></li>
+                        <li><img src="{{ URL::asset('image/c.jpeg') }}" alt="Third slide" height="200" width="200"></li>
+                        <li><img src="{{ URL::asset('image/c.jpeg') }}" alt="Third slide" height="200" width="200"></li>
+                        <li><img src="{{ URL::asset('image/c.jpeg') }}" alt="Third slide" height="200" width="200"></li>
+                        <li><img src="{{ URL::asset('image/c.jpeg') }}" alt="Third slide" height="200" width="200"></li>
+                        <li><img src="{{ URL::asset('image/c.jpeg') }}" alt="Third slide" height="200" width="200"></li>
+                        <li><img src="{{ URL::asset('image/c.jpeg') }}" alt="Third slide" height="200" width="200"></li>
+                    </ul>
+                </div>
+                <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+                <a class="next" onclick="plusSlides(1)">&#10095;</a>
+            </div>
+
+        </div>
+        <div class="row index-link " >
+            <div  class="col-md-3">
+                <a class="nav-link active" href="#">Active</a>
+            </div>
+
+            <div class="col-md-3">
+                <a class="nav-link" href="#">Link</a>
+            </div>
+            <div class="col-md-3">
+                <a class="nav-link" href="#">Link</a>
+            </div>
+            <div class="col-md-3">
+                <a class="nav-link" href="#">Disabled</a>
+            </div>
+        </div>
+        <p class="view-tab-title"> Danh mục sản phẩm</p>
+        @include('component.shop.view-tab-category')
+        <p class="view-tab-title"> Sản phẩm ưa chuộng</p>
+        @include('component.shop.highlights-item-shop')
+        <p class="view-tab-title"> Sản phẩm cao cấp</p>
+        @include('component.shop.luxury-item-shop')
+        <p class="view-tab-title">Liên Hệ</p>
+        @include('component.contact.supportEmail')
+    </div>
+
+@endsection
